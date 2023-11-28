832f08db6808817a61038e79a42d01333608d4f4
dedekm <dedekmm@gmail.com>
2023-11-28 21:06:24 +0100

    update for godot 4

--- FikApi.gd
+++ FikApi.gd
@@ -7,40 +7,40 @@ var current_data := {}
 var dirty := false
 var waiting_for_response := false
 
-onready var http_request := HTTPRequest.new()
-onready var timer := Timer.new()
+@onready var http_request := HTTPRequest.new()
+@onready var timer := Timer.new()
 
 func _ready():
-  http_request.connect("request_completed", self, "_on_request_completed")
+  http_request.request_completed.connect(self._on_request_completed)
   add_child(http_request)
 
   timer.set_wait_time(FREQUENCY)
   timer.set_one_shot(false)
-  timer.connect("timeout", self, "_update") 
+  timer.timeout.connect(self._update)
   add_child(timer)
   timer.start()
 
 
-func _update() -> void:
-  if dirty && !waiting_for_response:
+func _update():
+  if (dirty && !waiting_for_response):
     _send_post_request(current_data)
     dirty = false
     current_data = {}
 
 
-func _send_post_request(data_to_send: Dictionary) -> void:
-  var query := JSON.print(data_to_send)
+func _send_post_request(data_to_send: Dictionary):
+  var body := JSON.stringify(data_to_send)
   var headers := ["Content-Type: application/json"]
-  http_request.request(API_URL, headers, false, HTTPClient.METHOD_POST, query)
+  http_request.request(API_URL, headers, HTTPClient.METHOD_POST, body)
   waiting_for_response = true
 
 
-func _on_request_completed(_result, _response_code, _headers, _body) -> void:
+func _on_request_completed(_result, _response_code, _headers, _body):
   print(_response_code)
   waiting_for_response = false
 
 
-func set_data(data: Dictionary) -> void:
+func set_data(data: Dictionary):
   for key in data:
-      current_data[key] = data[key]
+    current_data[key] = data[key]
   dirty = true
